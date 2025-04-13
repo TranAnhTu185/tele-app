b68f9ec0f4c9f11061a90bf778cde3318b4369e4
HieuTd_Deptraivl <azhieuza@gmail.com>
2025-04-13 20:52:01 +0700

    hieutd add login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import type React from "react";
+import {Button} from "antd";
+
+const LoginPage: React.FC = () => {
+	
+	return (
+		<div className="flex flex-col items-center mt-22">
+			<Button type={"primary"}> Login</Button>
+		</div>
+	);
+};
+
+export default LoginPage;
