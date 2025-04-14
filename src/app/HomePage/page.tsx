ade2c9f7c38e86fce597871673c569832735381d
Lind <admin@Lind-2.local>
2025-04-14 11:20:19 +0700

    commi

--- src/app/HomePage/page.tsx
+++ src/app/HomePage/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import img1 from "../../../public/img-1.svg";
 import img11 from "../../../public/img-11.svg";
 import key from "../../../public/icons/key.svg";
@@ -10,16 +10,79 @@ import stats from "../../../public/btn/btn-stats.svg";
 import vuKhi from "../../../public/btn/btn-vk.svg";
 import monster from "../../../public/btn/monster.svg";
 import inventory from "../../../public/btn/inventory.svg";
-import {Button, Progress, Spin} from 'antd';
+import { Button, Progress, Spin } from 'antd';
+import Navbar from "../navbar/page";
+import Header from "../header/page";
 
 
 interface ChildProps {
     onButtonClick: (increment: string) => void;
-  }
+}
 
 export default function HomePage() {
     const [isStatic, setIsStatic] = useState("sum");
     const [loading, setLoading] = useState(false);
+    const [initData, setinitData] = useState<string | null>(null);
+    const [isAuTh, setisAuTh] = useState<boolean | null>(false);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        const tgApp = window.Telegram?.WebApp;
+        setTimeout(async () => {
+            if (tgApp) {
+                tgApp.ready();
+                if (tgApp.initData) {
+                    setinitData(tgApp.initData);
+                    try {
+                        const response = await fetch('https://ton-war.bytebuffer.co/auth', {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json',
+                            },
+                            body: JSON.stringify({
+                                "initData": tgApp.initData
+                            }),
+                        })
+                        if (!response.ok) {
+                            const errorData = await response.json();
+                            setisAuTh(false);
+                            setError(errorData);
+                            throw new Error(errorData.error || 'Failed to check membership');
+                        } else {
+                            const data = await response.json();
+                            console.log(data);
+                            try {
+                                const response = await fetch('https://ton-war.bytebuffer.co/account/me?text=day%20la%20gi%20%3F', {
+                                    method: 'GET',
+                                    headers: {
+                                        'Authorization': data.token,
+                                    },
+                                })
+                                if (!response.ok) {
+                                    const errorData = await response.json();
+                                    throw new Error(errorData.error || 'Failed to check');
+                                } else {
+                                    const data = await response.json();
+                                    console.log(data);
+                                }
+                            } catch (error) {
+                                console.error('Error loggin', error);
+                            } finally {
+                            }
+                            setisAuTh(true);
+                        }
+                    } catch (error) {
+                        console.error('Error loggin', error);
+                        setisAuTh(false);
+                        setError("login false");
+                    } finally {
+                    }
+                }
+            } else {
+                alert('No uesr login');
+            }
+        }, 500)
+    }, [])
 
     const handleChildClick = (increment: string) => {
         setLoading(true);
@@ -29,34 +92,54 @@ export default function HomePage() {
         }, 1000)
     };
     return (
-        <div className="text-white w-full mx-auto">
-            <div className="mt-22">
-                <div className="flex justify-between items-center m-3 rounded-[20px] pb-[3px] pt-[3px] pr-[8px] pl-[8px] bg-[#33321e]">
-                    <div className="mr-3">
-                        <Image
-                            src={icon1}
-                            alt=""
-                            className="w-[18px] h-[21px] mx-auto"
-                            style={{
-                                filter: "drop-shadow(0px 0px 24px #a726a9a8)",
-                            }}
-                        />
+        <main className="w-full">
+            {(initData && isAuTh == true) &&
+                <div className="w-full">
+                    <Header />
+                    <div className="text-white w-full mx-auto">
+                        <div className="mt-22">
+                            <div className="flex justify-between items-center m-3 rounded-[20px] pb-[3px] pt-[3px] pr-[8px] pl-[8px] bg-[#33321e]">
+                                <div className="mr-3">
+                                    <Image
+                                        src={icon1}
+                                        alt=""
+                                        className="w-[18px] h-[21px] mx-auto"
+                                        style={{
+                                            filter: "drop-shadow(0px 0px 24px #a726a9a8)",
+                                        }}
+                                    />
+                                </div>
+                                <div className="flex-1 text-xs text-left">Summon Monster and get 4 TON per day</div>
+                            </div>
+                            <div className="background-color-radi mx-[8px] border border-[rgba(255,255,255,0.4)]">
+                                <Spin spinning={loading}>
+                                    {isStatic === "stats" && <Statistic onButtonClick={handleChildClick} />}
+                                    {isStatic === "sum" && <SummonMonster onButtonClick={handleChildClick} />}
+                                    {isStatic === "weapon" && <Weapon onButtonClick={handleChildClick} />}
+                                </Spin>
+                            </div>
+                        </div>
                     </div>
-                    <div className="flex-1 text-xs text-left">Summon Monster and get 4 TON per day</div>
-                </div>
-                <div className="background-color-radi mx-[8px] border border-[rgba(255,255,255,0.4)]">
-                    <Spin spinning={loading}>
-                        {isStatic === "stats" && <Statistic onButtonClick={handleChildClick} />}
-                        {isStatic === "sum" && <SummonMonster onButtonClick={handleChildClick} />}
-                        {isStatic === "weapon" && <Weapon onButtonClick={handleChildClick} />}
-                    </Spin>
-                </div>
-            </div>
-        </div>
+                    <Navbar />
+                </div>}
+            {(!initData) && <NotUser dataString={initData + error} />}
+        </main>
     )
 }
 
 
+interface Props {
+    dataString: string | null;
+}
+
+function NotUser({ dataString }: Props) {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center p-4">
+            <h1 className="text-4xl font-bold mb-8">Ton War</h1>
+            <p className="text-xl">intData: {dataString}</p>
+        </div>
+    )
+}
 
 
 function SummonMonster({ onButtonClick }: ChildProps) {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,11 @@
 
 import { Button, Spin } from 'antd';
 import "./globals.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./page.style.css"
-import HomePage from './HomePage/page';
-import Header from './header/page';
-import Navbar from './navbar/page';
-
-interface Props {
-  dataString: string | null;
-}
-
+import Link from "next/link";
 interface ChildProps {
-	onButtonClick: (increment: boolean) => void;
+	onButtonClick: () => void;
 }
 
 interface TelegramWebApp {
@@ -30,76 +23,13 @@ declare global {
   }
 }
 export default function Home() {
-  const [initData, setinitData] = useState<string | null>(null);
-  const [isAuTh, setisAuTh] = useState<boolean | null>(false);
-  const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
-  const [login, setLogin] = useState(false);
-  useEffect(() => {
-    if (login == true) {
-      const tgApp = window.Telegram?.WebApp;
-      setTimeout(async () => {
-        if (tgApp) {
-          tgApp.ready();
-          if (tgApp.initData) {
-            setinitData(tgApp.initData);
-            try {
-              const response = await fetch('https://ton-war.bytebuffer.co/auth', {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                  "initData": tgApp.initData
-                }),
-              })
-              if (!response.ok) {
-                const errorData = await response.json();
-                setisAuTh(false);
-                setError(errorData);
-                throw new Error(errorData.error || 'Failed to check membership');
-              } else {
-                const data = await response.json();
-                console.log(data);
-                try {
-                  const response = await fetch('https://ton-war.bytebuffer.co/account/me?text=day%20la%20gi%20%3F', {
-                    method: 'GET',
-                    headers: {
-                      'Authorization': data.token,
-                    },
-                  })
-                  if (!response.ok) {
-                    const errorData = await response.json();
-                    throw new Error(errorData.error || 'Failed to check');
-                  } else {
-                    const data = await response.json();
-                    console.log(data);
-                  }
-                } catch (error) {
-                  console.error('Error loggin', error);
-                } finally {
-                }
-                setisAuTh(true);
-              }
-            } catch (error) {
-              console.error('Error loggin', error);
-              setisAuTh(false);
-              setError("login false");
-            } finally {
-            }
-          }
-        } else {
-          alert('No uesr login');
-        }
-      }, 1000)
-    }
-  }, [login])
+  
 
 
-  const handleChildClick = (increment: boolean) => {
+  const handleChildClick = () => {
     setLoading(true);
     setTimeout(() => {
-      setLogin(increment);
       setLoading(false);
     }, 1000)
   };
@@ -107,37 +37,18 @@ export default function Home() {
   return (
     <div>
       <Spin spinning={loading}>
-        {login == true &&
-          <div>
-            <Header />
-            <main className="w-full">
-              {(initData && isAuTh == true) && <HomePage />}
-              {(!initData) && <NotUser dataString={initData + error} />}
-            </main>
-            <Navbar />
-          </div>
-        }
-        {login == false && <LoginPage onButtonClick={handleChildClick} />}
+        <LoginPage onButtonClick={handleChildClick} />
       </Spin>
     </div>
 
   );
 }
 
-function NotUser({ dataString }: Props) {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center p-4">
-      <h1 className="text-4xl font-bold mb-8">Ton War</h1>
-      <p className="text-xl">intData: {dataString}</p>
-    </div>
-  )
-}
-
 function LoginPage({ onButtonClick }: ChildProps) {
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
-      <Button type={"primary"} onClick={() => onButtonClick(true)}> Login</Button>
-    </div>
+    <Link  href="/homePage" className="h-screen flex flex-col items-center justify-center">
+      <Button type={"primary"} onClick={() => onButtonClick()}> Login</Button>
+    </Link>
   );
 }
 
