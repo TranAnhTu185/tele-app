8c7093193e0a1897003a37b15d880ebcb3f5a29e
lind <trananhtu8900@gmail.com>
2025-05-06 18:31:58 +0700

    commit code

--- src/app/home/dataMonster/index.ts
+++ src/app/home/dataMonster/index.ts
@@ -1,18 +1,19 @@
-import Ashflare from "../../../../public/imgMonster/Ashflare.jpg";
-import Bazotuga from "../../../../public/imgMonster/Bazotuga.jpg";
-import Blastbeard from "../../../../public/imgMonster/Blastbeard.jpg";
-import Blazetail from "../../../../public/imgMonster/Blazetail.jpg";
-import Frostpaw from "../../../../public/imgMonster/Frostpaw.jpg";
-import Grizzleo from "../../../../public/imgMonster/Grizzleo.jpg";
-import Inkshade from "../../../../public/imgMonster/Inkshade.jpg";
-import Jokuro from "../../../../public/imgMonster/Jokuro.jpg";
-import Kitsuneko from "../../../../public/imgMonster/Kitsuneko.jpg";
-import Ruisaurus from "../../../../public/imgMonster/Ruisaurus.jpg";
+import Ashflare from "../../../../public/imgMonster/Ashflare.png";
+import Bazotuga from "../../../../public/imgMonster/Bazotuga.png";
+import Blastbeard from "../../../../public/imgMonster/Blastbeard.png";
+import Blazetail from "../../../../public/imgMonster/Blazetail.png";
+import Frostpaw from "../../../../public/imgMonster/Frostpaw.png";
+import Grizzleo from "../../../../public/imgMonster/Grizzleo.png";
+import Inkshade from "../../../../public/imgMonster/Inkshade.png";
+import Jokuro from "../../../../public/imgMonster/Jokuro.png";
+import Kitsuneko from "../../../../public/imgMonster/Kitsuneko.png";
+import Ruisaurus from "../../../../public/imgMonster/Ruisaurus.png";
 import Soulreign from "../../../../public/imgMonster/Soulreign.jpg";
-import Tridron from "../../../../public/imgMonster/Tridron.jpg";
-import Umbrashade from "../../../../public/imgMonster/Umbrashade.jpg";
-import Voltaron from "../../../../public/imgMonster/Voltaron.jpg";
-import froken from "../../../../public/imgMonster/froken.jpg";
+import Tridron from "../../../../public/imgMonster/Tridron.png";
+import Umbrashade from "../../../../public/imgMonster/Umbrashade.png";
+import Voltaron from "../../../../public/imgMonster/Voltaron.png";
+import froken from "../../../../public/imgMonster/froken.png";
+import Syrenix from "../../../../public/imgMonster/Syrenix.png";
 
 export const fixedData = [
     {
@@ -74,5 +75,9 @@ export const fixedData = [
     {
         name: "Froken",
         monsterImg: froken
+    },
+    {
+        name: "Syrenix",
+        monsterImg: Syrenix
     }
 ];
