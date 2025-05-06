01a8a61d581fa3450fa03eafe86a9073f0b18ca7
lind <trananhtu8900@gmail.com>
2025-05-06 14:37:07 +0700

    commit code wallet

--- src/app/wallet/page.tsx
+++ src/app/wallet/page.tsx
@@ -5,14 +5,16 @@ import { FileTextOutlined, LinkOutlined, RedoOutlined } from "@ant-design/icons"
 import diamond from "../../../public/icons/diamond.svg";
 import wallet from "../../../public/icons/wallet.svg";
 import Image from "next/image";
-import { useTonConnectUI } from "@tonconnect/ui-react";
+import { useTonAddress, useTonConnectUI, useTonWallet } from "@tonconnect/ui-react";
 import { useCallback, useEffect, useState } from "react";
-import { Address } from "@ton/core";
+import { Address, beginCell, Cell, toNano } from "@ton/core";
 import { DataUser } from "../utils/common";
 import { getFromLocalStorage } from "../utils/localStorage";
+import { sendTonTransaction } from "../lib/ton";
 
 const WalletPage: React.FC = () => {
     const [tonConnectUI] = useTonConnectUI();
+    const userFriendlyAddress = useTonAddress();
     const [tonWalletAddress, setTonWalletAddress] = useState<string | null>(null);
     const [isLoading, setIsLoading] = useState(true);
 
@@ -77,6 +79,10 @@ const WalletPage: React.FC = () => {
     const handleClickTran = (tab: string) => {
         setTabShow(tab);
     }
+
+    const handleChildClick = (increment: string) => {
+        setTabShow(increment);
+    };
 
     if (isLoading) {
         return (
@@ -175,8 +181,8 @@ const WalletPage: React.FC = () => {
                             </div>
                         </div>
                     </div>}
-                    {tabShow === 'deposit' && <div>deposit</div>}
-                    {tabShow === 'withdraw' && <Withdraw/>}
+                    {tabShow === 'deposit' && <Deposit address={userFriendlyAddress} onButtonClick={handleChildClick} />}
+                    {tabShow === 'withdraw' && <Withdraw address={userFriendlyAddress} onButtonClick={handleChildClick} />}
                 </main>
             ) : (
                 <main className="pb-[120px] background-color-main  min-h-[100vh] pt-[40px] relative flex items-center justify-center">
@@ -196,82 +202,249 @@ const WalletPage: React.FC = () => {
     );
 };
 
+type Props = {
+    address: string;
+    onButtonClick: (increment: string) => void;
+};
 
-const Withdraw = () => {
+function Withdraw({ address, onButtonClick }: Props) {
+
+    const [formData, setFormData] = useState({
+        amount: 0,
+        code: '',
+        address: '',
+        fee: 10000,
+    });
+
+    const handeleSendCode = async () => {
+        const stored = localStorage.getItem('token');
+        if (stored !== null && stored !== undefined) {
+            try {
+                const response = await fetch('https://ton-war.bytebuffer.co/otp', {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': JSON.parse(stored),
+                    },
+                })
+                if (response.ok) {
+                    const dataTest = await response.json();
+                    console.log(dataTest);
+                }
+            } catch (error) {
+                console.error('GET failed:', error);
+            }
+        } else {
+            console.error("no token");
+        }
+    }
+
+    const handleWithdraw = async () => {
+        formData.address = address;
+        console.log(formData);
+        const stored = localStorage.getItem('token');
+        if (stored !== null && stored !== undefined) {
+            try {
+                const response = await fetch('https://ton-war.bytebuffer.co/withdraw', {
+                    method: 'POST',
+                    headers: {
+                        'Authorization': JSON.parse(stored),
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        "amount": Number(formData.amount),
+                        "wallet": address,
+                        "otp": formData.code
+                    }),
+                })
+                if (response.ok) {
+                    const dataTest = await response.json();
+                    console.log(dataTest);
+                }
+            } catch (error) {
+                console.error('GET failed:', error);
+            }
+        } else {
+            console.error("no token");
+        }
+    }
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData(prev => ({ ...prev, [name]: value }));
+    };
     return (
-      <div className="min-h-screen text-white flex flex-col items-center p-6">
-        {/* Header */}
-        <div className="w-full max-w-md flex items-center justify-between text-xl font-semibold mb-6">
-          <button className="text-white text-2xl">{'<'} </button>
-          <h1 className="text-center flex-1">Withdraw <span className="ml-1">💎</span></h1>
-          <div className="w-6" /> {/* placeholder to balance the back button */}
+        <div className="min-h-screen text-white flex flex-col items-center p-6">
+            {/* Header */}
+            <div className="w-full max-w-md flex items-center justify-between text-xl font-semibold mb-6">
+                <button className="text-white text-2xl cursor-pointer grounded-radiants-input w-[40px] h-[40px] flex items-center justify-center" onClick={() => onButtonClick("home")}>{'<'} </button>
+                <h1 className="text-center flex-1">Withdraw <span className="ml-1">💎</span></h1>
+                <div className="w-6" /> {/* placeholder to balance the back button */}
+            </div>
+
+            {/* Main Card */}
+            <div className="w-full max-w-md p-6 rounded-2xl space-y-5">
+
+                {/* Address */}
+                <div>
+                    <label className="text-sm mb-1 block">Address</label>
+                    <input
+                        disabled
+                        className="w-full grounded-radiants-input text-gray-400 p-3 rounded-lg"
+                        value={Address.parse(address).toString()}
+                    />
+                </div>
+
+                {/* Amount */}
+                <div>
+                    <div className="flex justify-between mb-1">
+                        <label className="text-sm">Amount</label>
+                        <span className="text-sm text-gray-400">Available 0.04 TON</span>
+                    </div>
+                    <div className="relative">
+                        <input
+                            name="amount"
+                            className="w-full grounded-radiants-input p-3 pr-16 rounded-lg focus:outline-none"
+                            placeholder="0"
+                            type="number"
+                            value={formData.amount}
+                            onChange={handleChange}
+                        />
+                        <button className="absolute cursor-pointer right-2 top-1/2 -translate-y-1/2 text-sm text-white background-color-gra-green px-3 py-1 rounded-lg">
+                            MAX
+                        </button>
+                    </div>
+                </div>
+
+                {/* Code */}
+                <div>
+                    <label className="text-sm mb-1 block">Code</label>
+                    <div className="relative">
+                        <input
+                            className="w-full grounded-radiants-input p-3 pr-28 rounded-lg focus:outline-none"
+                            placeholder=""
+                            type="text"
+                            name="code"
+                            value={formData.code}
+                            onChange={handleChange}
+                        />
+                        <button className="absolute cursor-pointer right-2 top-1/2 -translate-y-1/2 text-sm text-white background-color-gra-green px-3 py-1 rounded-lg" onClick={handeleSendCode}>
+                            Send code
+                        </button>
+                    </div>
+                </div>
+
+                {/* Fee */}
+                {/* <div>
+                    <div className="flex justify-between text-sm text-gray-400 mb-1">
+                        <span>Withdraw Fee</span>
+                    </div>
+                    <input
+                        disabled
+                        className="w-full grounded-radiants-input text-gray-400 p-3 rounded-lg"
+                        value={formData.fee}
+                        onChange={handleChange}
+                    />
+                </div> */}
+
+                {/* Withdraw Button */}
+                <button onClick={handleWithdraw} className="w-full cursor-pointer py-3 rounded-full text-white font-semibold background-color-gra-green">
+                    Withdraw
+                </button>
+            </div>
         </div>
-  
-        {/* Main Card */}
-        <div className="w-full max-w-md p-6 rounded-2xl space-y-5">
-  
-          {/* Address */}
-          <div>
-            <label className="text-sm mb-1 block">Address</label>
-            <input
-              disabled
-              className="w-full grounded-radiants-input text-gray-400 p-3 rounded-lg"
-              value="UQDsUYVQUuX2K-ZWggy...P4asuJj"
-            />
-          </div>
-  
-          {/* Amount */}
-          <div>
-            <div className="flex justify-between mb-1">
-              <label className="text-sm">Amount</label>
-              <span className="text-sm text-gray-400">Available 0.04 TON</span>
-            </div>
-            <div className="relative">
-              <input
-                className="w-full grounded-radiants-input p-3 pr-16 rounded-lg focus:outline-none"
-                placeholder="0"
-                type="number"
-              />
-              <button className="absolute right-2 top-1/2 -translate-y-1/2 text-sm text-white background-color-gra-green px-3 py-1 rounded-lg">
-                MAX
-              </button>
-            </div>
-          </div>
-  
-          {/* Code */}
-          <div>
-            <label className="text-sm mb-1 block">Code</label>
-            <div className="relative">
-              <input
-                className="w-full grounded-radiants-input p-3 pr-28 rounded-lg focus:outline-none"
-                placeholder=""
-                type="text"
-              />
-              <button className="absolute right-2 top-1/2 -translate-y-1/2 text-sm text-white background-color-gra-green px-3 py-1 rounded-lg">
-                Send code
-              </button>
-            </div>
-          </div>
-  
-          {/* Fee */}
-          <div>
-            <div className="flex justify-between text-sm text-gray-400 mb-1">
-              <span>Withdraw Fee</span>
-            </div>
-            <input
-              disabled
-              className="w-full grounded-radiants-input text-gray-400 p-3 rounded-lg"
-              value="0 ebh"
-            />
-          </div>
-  
-          {/* Withdraw Button */}
-          <button className="w-full py-3 rounded-full text-white font-semibold background-color-gra-green">
-            Withdraw
-          </button>
-        </div>
-      </div>
     );
-  };
+};
+
+
+function Deposit({ address, onButtonClick }: Props) {
+    const [tonConnectUI] = useTonConnectUI();
+    const addressNhan = "0QDqpyz11y8BMxUi-PNUxgTdLiLIjFV1IcC3-1Bsc3Gt095c";
+    const [formData, setFormData] = useState({
+        amount: 0,
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData(prev => ({ ...prev, [name]: value }));
+    };
+
+    const handleDeposit = async () => {
+        try {
+            const stored = getFromLocalStorage<DataUser>('userInfo');
+            debugger;
+            if (stored) {
+                const payload = beginCell()
+                    .storeUint(0, 32)
+                    .storeStringTail(stored.userId ? stored.userId : "")
+                    .endCell();
+                const result = await tonConnectUI.sendTransaction({
+                    validUntil: Math.floor(Date.now() / 1000) + 600,
+                    messages: [
+                        {
+                            address: addressNhan, // địa chỉ nhận
+                            amount: toNano(formData.amount).toString(), // 0.1 TON = 100M nanoTON
+                            payload: payload.toBoc().toString("base64"),
+                        },
+                    ],
+                })
+                console.log("result: " + JSON.stringify(result));
+                const bocBase64 = result.boc;
+                const cell = Cell.fromBase64(bocBase64);
+                const slice = cell.beginParse();
+                console.log(slice);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    return (
+        <div className="min-h-screen text-white flex flex-col items-center p-6">
+            {/* Header */}
+            <div className="w-full max-w-md flex items-center justify-between text-xl font-semibold mb-6">
+                <button className="text-white text-2xl cursor-pointer grounded-radiants-input w-[40px] h-[40px] flex items-center justify-center" onClick={() => onButtonClick("home")}>{'<'} </button>
+                <h1 className="text-center flex-1">Deposit <span className="ml-1">💎</span></h1>
+                <div className="w-6" /> {/* placeholder to balance the back button */}
+            </div>
+
+            {/* Main Card */}
+            <div className="w-full max-w-md p-6 rounded-2xl space-y-5">
+
+                {/* Address */}
+                <div>
+                    <label className="text-sm mb-1 block">Address</label>
+                    <input
+                        disabled
+                        className="w-full grounded-radiants-input text-gray-400 p-3 rounded-lg"
+                        value={Address.parse(addressNhan).toString()}
+                    />
+                </div>
+
+                {/* Amount */}
+                <div>
+                    <div className="flex justify-between mb-1">
+                        <label className="text-sm">Amount</label>
+                        <span className="text-sm text-gray-400">Available 0.04 TON</span>
+                    </div>
+                    <div className="relative">
+                        <input
+                            name="amount"
+                            className="w-full grounded-radiants-input p-3 pr-16 rounded-lg focus:outline-none"
+                            placeholder="0"
+                            type="number"
+                            value={formData.amount}
+                            onChange={handleChange}
+                        />
+                        <button className="absolute cursor-pointer right-2 top-1/2 -translate-y-1/2 text-sm text-white background-color-gra-green px-3 py-1 rounded-lg">
+                            MAX
+                        </button>
+                    </div>
+                </div>
+                <button onClick={handleDeposit} className="w-full cursor-pointer py-3 rounded-full text-white font-semibold background-color-gra-green">
+                    Deposit
+                </button>
+            </div>
+        </div>
+    );
+};
 
 export default WalletPage;
