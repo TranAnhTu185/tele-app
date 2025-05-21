fb886c9dd02f5719bb3c83d4fc53f8feeea3f4aa
lind <trananhtu8900@gmail.com>
2025-05-21 09:00:51 +0700

    commit fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { Outfit } from "next/font/google";
 import Script from "next/script";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
-const inter = Inter({ subsets: ["latin"] });
+const out = Outfit({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -20,7 +20,7 @@ export default function RootLayout({
         <title>TON War</title>
         <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
       </head>
-      <body className={inter.className + " bg-black"}>
+      <body className={out.className + " bg-black"}>
         <TonConnectUIProvider manifestUrl="https://teal-defeated-piranha-723.mypinata.cloud/ipfs/bafkreicratyikzcjr73lr6v3yvp7u4ne7bqnkimgj7ogtavei4lzabdxga">
           {children}
         </TonConnectUIProvider>
